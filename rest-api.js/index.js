53fee46629a6a37d5f57b4fc56a62168652f9432
Simee Pandey <164655969+simee56@users.noreply.github.com>
2025-01-03 00:03:42 +0530

    Update index.js

--- rest-api.js/index.js
+++ rest-api.js/index.js
@@ -4,7 +4,8 @@ const users = require('./MOCK_DATA.json');
 const app = express();
 const PORT = 8000;
 
-app.get("/users", (req,res) => {
+//User's information in html format
+app.get("/users", (req, res) => {
     const html = `
     <ul>
         ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
@@ -12,18 +13,38 @@ app.get("/users", (req,res) => {
     res.end(html);
 })
 
-app.get("/api/users/:id", (req,res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id ===id );
-    return res.json(user)
-});
-
-app.get("/api/users/:id", (req,res) => {
+//User's information in json format
+app.get("/api/users", (req, res) => {
     return res.json(users)
 });
 
 
+//Finding user by id
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user)
+});
+
+
+//Creating a new account for a new user
+app.post("/api/users", (req, res) => {
+    return res.json({ status: "pending" })
+});
+
+
+//Updating inforamtion of an existing user
+app.patch("/api/users/:id", (req, res) => {
+    return res.json({ status: "pending" })
+});
+
+
+//Deleting an user's aocount
+app.delete("/api/users/:id", (req, res) => {
+    return res.json({ status: "pending" })
+});
 
 app.listen(PORT, () => {
     console.log(`Server Started at port ${PORT}`)
 });
+
