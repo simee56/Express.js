4db4207cb2e3bbb09978e348ad71b578432b38c6
simee56 <pandeysimee7@gmail.com>
2025-01-02 23:57:38 +0530

    index.js

--- rest-api.js/index.js
+++ rest-api.js/index.js
@@ -5,8 +5,23 @@ const app = express();
 const PORT = 8000;
 
 app.get("/users", (req,res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+    res.end(html);
+})
+
+app.get("/api/users/:id", (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id ===id );
+    return res.json(user)
+});
+
+app.get("/api/users/:id", (req,res) => {
     return res.json(users)
 });
+
 
 
 app.listen(PORT, () => {
